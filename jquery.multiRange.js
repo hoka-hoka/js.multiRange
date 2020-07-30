d58571470d6850d7575ef9425652170ae4b9c81c
semyon <sff1@tpu.ru>
2020-07-30 18:08:27 +0700

    addRange

--- jquery.multiRange.js
+++ jquery.multiRange.js
@@ -1,5 +1,5 @@
 (function( $ ) {
-  var namespace = 'multiRange';
+  var namespace = 'multirange';
 
   class EventEmitter {
     constructor() {
@@ -10,9 +10,11 @@
       return this;
     }
     emit(evt, arg) {
+      let a = [];
       (this._events[evt] || []).slice().forEach(lsn => {
-        lsn(arg);
+        a.push(lsn(arg)); // () => this.Function()
       });
+      return a;
     }
   };
 
@@ -24,7 +26,8 @@
 
     init() {
 
-      let current = this._options.range;
+      let current = this._options.range[0];
+
       let min = current.min || 0;
       let max = current.max || 100;
       let value = current.getAttribute('value');
@@ -36,12 +39,31 @@
         let thumb = $('<div class="thumb">');
         current.addClass('basic');
         ghost.addClass('ghost');
+        this._options.ghost = ghost;
 
-        current.val(values[0] || min + (max - min) / 2);
-        ghost.val(values[1] || min + (max - min) / 2);
+        const expr =  min + (max - min) / 2;
+
+        current.attr('value', values[0] || expr);
+        current.prop({value: values[0] || expr}); // !!!
+        ghost.attr('value', values[1] || expr);
+        ghost.prop({value: values[1] || expr}); // !!!
 
         this.emit('addRange', {current, add: [ghost, thumb]});
+        this.emit('moveRange')[0];
+        this.emit('update', {
+          thumb,
+          pos: [getPercent(this._options.valueLow), getPercent(this._options.valueHight)],
+        });
+        console.log(this._options.valueLow);
+        function getPercent(value) {
+          return 100 * ((value - min) / (max - min)) + '%';
+        }
       }
+    }
+    followProperty() {
+      let opt = this._options;
+      opt.valueLow = Math.min(opt.range.val(), opt.ghost.val())
+      opt.valueHight = Math.max(opt.range.val(), opt.ghost.val());
     }
   };
 
@@ -49,10 +71,14 @@
     constructor(model) {
       super();
       this._model = model;
-
     }
     addRange(range, add) {
       range.current.after(add);
+    }
+    update(el) {
+      console.log(el.pos);
+      el.thumb.css("--start", el.pos[0]);
+      el.thumb.css("--stop", el.pos[1]);
     }
   };
 
@@ -62,12 +88,24 @@
       this._model = model;
       this._view = view;
       this._model.on('addRange', (r) => this.addRange(r));
+      this._model.on('moveRange', () => this.moveRange());
+      this._model.on('update', (t) => this.update(t));
+
+
     }
     addRange(range) {
       range.add.map(v => {
         this._view.addRange(range, v);
       });
-
+    }
+    moveRange() {
+      $('input.' + namespace).on('input', (e) => {
+        this._model.followProperty();
+        e.stopPropagation();
+      });
+    }
+    update(el) {
+      this._view.update(el);
     }
   };
 
@@ -75,7 +113,7 @@
     this.map((i, v) => {
       let options = {
         index: i,
-        range: v
+        range: $(v)
       };
       const model = new ListModel(options);
       const view = new ListView(model);
--- text.js
+++ text.js
@@ -1,16 +1,6 @@
 (function($) {
-  class ListModel {
-    constructor(name) {
-      this.name = name
-    }
-    getName() {
-      console.log(this.name);
-    }
-  }
-  window.addEventListener('load', () => {
-    const model = new ListModel('Vasia');
-    console.log(model.getName());
-  });
+  let a = document.querySelector('.multirange');
+  console.log(a.value);
 
 
 })(jQuery);
